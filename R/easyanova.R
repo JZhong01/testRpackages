6e22153b6483d27a2e7b0d08699964d9c97f2a8f
JZhong01 <51939865+JZhong01@users.noreply.github.com>
2023-11-12 14:40:02 -0500

    Update easyanova.R

--- R/easyanova.R
+++ R/easyanova.R
@@ -2,43 +2,50 @@
 ## utility for anova.FOO(), FOO in "lmlist", "glm", "glmlist"
 ## depending on the ordering of the models this might get called with
 ## negative deviance and df changes.
-stat.anova <-
-  function(table, test=c("Rao","LRT","Chisq", "F", "Cp"), scale, df.scale, n)
-  {
+stat.anova <- function(table, test = c("Rao", "LRT", "Chisq", "F", "Cp"), scale, df.scale, n) {
     test <- match.arg(test)
     dev.col <- match("Deviance", colnames(table))
     if (test == "Rao") dev.col <- match("Rao", colnames(table))
     if (is.na(dev.col)) dev.col <- match("Sum of Sq", colnames(table))
-    switch(test,
-           "Rao" = ,"LRT" = ,"Chisq" = {
-             dfs <- table[, "Df"]
-             vals <- table[, dev.col]/scale * sign(dfs)
-             vals[dfs %in% 0] <- NA
-             vals[!is.na(vals) & vals < 0] <- NA # rather than p = 0
-             cbind(table,
-                   "Pr(>Chi)" = pchisq(vals, abs(dfs), lower.tail = FALSE)
-             )
-           },
-           "F" = {
-             dfs <- table[, "Df"]
-             Fvalue <- (table[, dev.col]/dfs)/scale
-             Fvalue[dfs %in% 0] <- NA
-             Fvalue[!is.na(Fvalue) & Fvalue < 0] <- NA # rather than p = 0
-             cbind(table,
-                   F = Fvalue,
-                   "Pr(>F)" = pf(Fvalue, abs(dfs), df.scale, lower.tail = FALSE)
-             )
-           },
-           "Cp" = { # depends on the type of object.
-             if ("RSS" %in% names(table)) { # an lm object
-               cbind(table, Cp = table[, "RSS"] +
-                       2*scale*(n - table[, "Res.Df"]))
-             } else { # a glm object
-               cbind(table, Cp = table[, "Resid. Dev"] +
-                       2*scale*(n - table[, "Resid. Df"]))
-             }
-           })
-  }
+
+    switch(
+        test,
+        "Rao" = ,
+        "LRT" = ,
+        "Chisq" = {
+            dfs <- table[, "Df"]
+            vals <- table[, dev.col] / scale * sign(dfs)
+            vals[dfs %in% 0] <- NA
+            vals[!is.na(vals) & vals < 0] <- NA  # rather than p = 0
+            total_row <- c("Total", sum(vals, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
+            rbind(
+                table,
+                cbind("Pr(>Chi)" = pchisq(vals, abs(dfs), lower.tail = FALSE), table)
+            )
+        },
+        "F" = {
+            dfs <- table[, "Df"]
+            Fvalue <- (table[, dev.col] / dfs) / scale
+            Fvalue[dfs %in% 0] <- NA
+            Fvalue[!is.na(Fvalue) & Fvalue < 0] <- NA  # rather than p = 0
+            total_row <- c("Total", sum(Fvalue, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
+            rbind(
+                table,
+                cbind(F = Fvalue, "Pr(>F)" = pf(Fvalue, abs(dfs), df.scale, lower.tail = FALSE), table)
+            )
+        },
+        "Cp" = {
+            if ("RSS" %in% names(table)) {  # an lm object
+                total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
+            } else {  # a glm object
+                total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
+            }
+            rbind(table, total_row)
+        }
+    )
+}
+
+
 
 printCoefmat <-
   function(x, digits = max(3L, getOption("digits") - 2L),
@@ -176,7 +183,7 @@ print.anova <- function(x, digits = max(getOption("digits") - 2L, 3L),
   # Print the total row
   cat("\nTotal:\n")
   print.default(data.frame(total_row), digits = digits,
-                right = TRUE, quote = FALSE, row.names = FALSE)
-
+                  right = TRUE, quote = FALSE, row.names = FALSE)
+    
   invisible(x)
-}
+}             
