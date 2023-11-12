dce8bf6a337a9021bd2f7b89464869df603cccf3
JZhong01 <51939865+JZhong01@users.noreply.github.com>
2023-11-12 18:36:04 -0500

    print in coefmat function

--- R/easyanova.R
+++ R/easyanova.R
@@ -47,70 +47,48 @@ stat.anova <- function(table, test = c("Rao", "LRT", "Chisq", "F", "Cp"), scale,
 
 
 
-printCoefmat <-
-  function(x, digits = max(3L, getOption("digits") - 2L),
-           signif.stars = getOption("show.signif.stars"),
-           signif.legend = signif.stars,
-           dig.tst = max(1L, min(5L, digits - 1L)),
-           cs.ind = 1:k, tst.ind = k+1, zap.ind = integer(),
-           P.values = NULL,
-           has.Pvalue = nc >= 4L && length(cn <- colnames(x)) &&
-             substr(cn[nc], 1L, 3L) %in% c("Pr(", "p-v"),
-           eps.Pvalue = .Machine$double.eps,
-           na.print = "NA", quote = FALSE, right = TRUE, ...)
-  {
-    ## For printing ``coefficient matrices'' as they are in summary.xxx(.) where
-    ## xxx in {lm, glm, aov, ..}. (Note: summary.aov(.) gives a class "anova").
-
-    ## By Default
-    ## Assume: x is a matrix-like numeric object.
-    ## ------  with *last* column = P-values  --iff-- P.values (== TRUE)
-    ##	  columns {cs.ind}= numbers, such as coefficients & std.err  [def.: 1L:k]
-    ##	  columns {tst.ind}= test-statistics (as "z", "t", or "F")  [def.: k+1]
-
+printCoefmat <- function(x, digits = max(3L, getOption("digits") - 2L), signif.stars = getOption("show.signif.stars"), signif.legend = signif.stars, dig.tst = max(1L, min(5L, digits - 1L)), cs.ind = 1:k, tst.ind = k+1, zap.ind = integer(), P.values = NULL, has.Pvalue = nc >= 4L && length(cn <- colnames(x)) && substr(cn[nc], 1L, 3L) %in% c("Pr(", "p-v"), eps.Pvalue = .Machine$double.eps, na.print = "NA", quote = FALSE, right = TRUE, ...) {
     if(is.null(d <- dim(x)) || length(d) != 2L)
-      stop("'x' must be coefficient matrix/data frame")
+        stop("'x' must be a coefficient matrix/data frame")
     nc <- d[2L]
     if(is.null(P.values)) {
-      scp <- getOption("show.coef.Pvalues")
-      if(!is.logical(scp) || is.na(scp)) {
-        warning("option \"show.coef.Pvalues\" is invalid: assuming TRUE")
-        scp <- TRUE
-      }
-      P.values <- has.Pvalue && scp
+        scp <- getOption("show.coef.Pvalues")
+        if(!is.logical(scp) || is.na(scp)) {
+            warning("option \"show.coef.Pvalues\" is invalid: assuming TRUE")
+            scp <- TRUE
+        }
+        P.values <- has.Pvalue && scp
     }
     else if(P.values && !has.Pvalue)
-      stop("'P.values' is TRUE, but 'has.Pvalue' is not")
+        stop("'P.values' is TRUE, but 'has.Pvalue' is not")
 
-    if(has.Pvalue && !P.values) {# P values are there, but not wanted
-      d <- dim(xm <- data.matrix(x[,-nc , drop = FALSE]))
-      nc <- nc - 1
-      has.Pvalue <- FALSE
+    if(has.Pvalue && !P.values) {  # P values are there, but not wanted
+        d <- dim(xm <- data.matrix(x[,-nc , drop = FALSE]))
+        nc <- nc - 1
+        has.Pvalue <- FALSE
     } else xm <- data.matrix(x)
 
     k <- nc - has.Pvalue - (if(missing(tst.ind)) 1 else length(tst.ind))
     if(!missing(cs.ind) && length(cs.ind) > k) stop("wrong k / cs.ind")
 
-    Cf <- array("", dim=d, dimnames = dimnames(xm))
+    Cf <- array("", dim = d, dimnames = dimnames(xm))
 
     ok <- !(ina <- is.na(xm))
     ## zap before deciding any formats
     for (i in zap.ind) xm[, i] <- zapsmall(xm[, i], digits)
     if(length(cs.ind)) {
-      acs <- abs(coef.se <- xm[, cs.ind, drop=FALSE])# = abs(coef. , stderr)
-      if(any(ia <- is.finite(acs))) {
-        ## #{digits} BEFORE decimal point -- for min/max. value:
-        digmin <- 1 + if(length(acs <- acs[ia & acs != 0]))
-          floor(log10(range(acs[acs != 0], finite = TRUE))) else 0
-        Cf[,cs.ind] <- format(round(coef.se, max(1L, digits - digmin)),
-                              digits = digits)
-      }
+        acs <- abs(coef.se <- xm[, cs.ind, drop = FALSE])  # = abs(coef. , stderr)
+        if(any(ia <- is.finite(acs))) {
+            ## #{digits} BEFORE decimal point -- for min/max. value:
+            digmin <- 1 + if(length(acs <- acs[ia & acs != 0]))
+                floor(log10(range(acs[acs != 0], finite = TRUE))) else 0
+            Cf[,cs.ind] <- format(round(coef.se, max(1L, digits - digmin)), digits = digits)
+        }
     }
     if(length(tst.ind))
-      Cf[, tst.ind] <- format(round(xm[, tst.ind], digits = dig.tst),
-                              digits = digits)
+        Cf[, tst.ind] <- format(round(xm[, tst.ind], digits = dig.tst), digits = digits)
     if(any(r.ind <- !((1L:nc) %in% c(cs.ind, tst.ind, if(has.Pvalue) nc))))
-      for(i in which(r.ind)) Cf[, i] <- format(xm[, i], digits = digits)
+        for(i in which(r.ind)) Cf[, i] <- format(xm[, i], digits = digits)
     ok[, tst.ind] <- FALSE
     okP <- if(has.Pvalue) ok[, -nc] else ok
     ## we need to find out where Cf is zero.  We can't use as.numeric
@@ -121,40 +99,43 @@ printCoefmat <-
     if(dec != ".") x1 <- chartr(dec, ".", x1)
     x0 <- (xm[okP] == 0) != (as.numeric(x1) == 0)
     if(length(not.both.0 <- which(x0 & !is.na(x0)))) {
-      ## not.both.0==TRUE:  xm !=0, but Cf[] is: --> fix these:
-      Cf[okP][not.both.0] <- format(xm[okP][not.both.0],
-                                    digits = max(1L, digits - 1L))
+        ## not.both.0==TRUE:  xm !=0, but Cf[] is: --> fix these:
+        Cf[okP][not.both.0] <- format(xm[okP][not.both.0], digits = max(1L, digits - 1L))
     }
     if(any(ina)) Cf[ina] <- na.print
     if(any(inan <- is.nan(xm))) Cf[inan] <- "NaN"  # PR#17336
     if(P.values) {
-      if(!is.logical(signif.stars) || is.na(signif.stars)) {
-        warning("option \"show.signif.stars\" is invalid: assuming TRUE")
-        signif.stars <- TRUE
-      }
-      if(any(okP <- ok[,nc])) {
-        pv <- as.vector(xm[, nc]) # drop names
-        Cf[okP, nc] <- format.pval(pv[okP],
-                                   digits = dig.tst, eps = eps.Pvalue)
-        signif.stars <- signif.stars && any(pv[okP] < .1)
-        if(signif.stars) {
-          Signif <- symnum(pv, corr = FALSE, na = FALSE,
-                           cutpoints = c(0,  .001,.01,.05, .1, 1),
-                           symbols   =  c("***","**","*","."," "))
-          Cf <- cbind(Cf, format(Signif)) #format.ch: right=TRUE
+        if(!is.logical(signif.stars) || is.na(signif.stars)) {
+            warning("option \"show.signif.stars\" is invalid: assuming TRUE")
+            signif.stars <- TRUE
         }
-      } else signif.stars <- FALSE
+        if(any(okP <- ok[,nc])) {
+            pv <- as.vector(xm[, nc])  # drop names
+            Cf[okP, nc] <- format.pval(pv[okP], digits = dig.tst, eps = eps.Pvalue)
+            signif.stars <- signif.stars && any(pv[okP] < .1)
+            if(signif.stars) {
+                Signif <- symnum(pv, corr = FALSE, na = FALSE,
+                                 cutpoints = c(0,  .001,.01,.05, .1, 1),
+                                 symbols   =  c("***","**","*","."," "))
+                Cf <- cbind(Cf, format(Signif))  # format.ch: right=TRUE
+            }
+        } else signif.stars <- FALSE
     } else signif.stars <- FALSE
+
+    # Add total row to the matrix
+    total_row <- c("Total", colSums(x[, zap.ind], na.rm = TRUE))
+    Cf <- rbind(Cf, total_row)
+
     print.default(Cf, quote = quote, right = right, na.print = na.print, ...)
     if(signif.stars && signif.legend) {
-      if((w <- getOption("width")) < nchar(sleg <- attr(Signif,"legend")))# == 46
-        sleg <- strwrap(sleg, width = w - 2, prefix = "  ")
-      ##"FIXME": Double space __ is for reproducibility, rather than by design
-      cat("---\nSignif. codes:  ", sleg, sep = "",
-          fill = w+4 + max(nchar(sleg,"bytes") - nchar(sleg)))# +4: "---"
+        if((w <- getOption("width")) < nchar(sleg <- attr(Signif,"legend")))# == 46
+            sleg <- strwrap(sleg, width = w - 2, prefix = "  ")
+        ##"FIXME": Double space __ is for reproducibility, rather than by design
+        cat("---\nSignif. codes:  ", sleg, sep = "", fill = w+4 + max(nchar(sleg,"bytes") - nchar(sleg)))# +4: "---"
     }
     invisible(x)
-  }
+}
+
 
 print.anova <- function(x, digits = max(getOption("digits") - 2L, 3L),
                         signif.stars = getOption("show.signif.stars"), ...)
