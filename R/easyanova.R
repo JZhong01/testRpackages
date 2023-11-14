c9255be00f61944c22cd3d743eaab612339bf75e
JZhong01 <51939865+JZhong01@users.noreply.github.com>
2023-11-14 18:00:45 -0500

    print total row in matrix

--- R/easyanova.R
+++ R/easyanova.R
@@ -3,46 +3,53 @@
 ## depending on the ordering of the models this might get called with
 ## negative deviance and df changes.
 stat.anova <- function(table, test = c("Rao", "LRT", "Chisq", "F", "Cp"), scale, df.scale, n) {
-    test <- match.arg(test)
-    dev.col <- match("Deviance", colnames(table))
-    if (test == "Rao") dev.col <- match("Rao", colnames(table))
-    if (is.na(dev.col)) dev.col <- match("Sum of Sq", colnames(table))
+  test <- match.arg(test)
+  dev.col <- match("Deviance", colnames(table))
+  if (test == "Rao") dev.col <- match("Rao", colnames(table))
+  if (is.na(dev.col)) dev.col <- match("Sum of Sq", colnames(table))
 
-    switch(
-        test,
-        "Rao" = ,
-        "LRT" = ,
-        "Chisq" = {
-            dfs <- table[, "Df"]
-            vals <- table[, dev.col] / scale * sign(dfs)
-            vals[dfs %in% 0] <- NA
-            vals[!is.na(vals) & vals < 0] <- NA  # rather than p = 0
-            total_row <- c("Total", sum(vals, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
-            rbind(
-                table,
-                cbind("Pr(>Chi)" = pchisq(vals, abs(dfs), lower.tail = FALSE), table)
-            )
-        },
-        "F" = {
-            dfs <- table[, "Df"]
-            Fvalue <- (table[, dev.col] / dfs) / scale
-            Fvalue[dfs %in% 0] <- NA
-            Fvalue[!is.na(Fvalue) & Fvalue < 0] <- NA  # rather than p = 0
-            total_row <- c("Total", sum(Fvalue, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
-            rbind(
-                table,
-                cbind(F = Fvalue, "Pr(>F)" = pf(Fvalue, abs(dfs), df.scale, lower.tail = FALSE), table)
-            )
-        },
-        "Cp" = {
-            if ("RSS" %in% names(table)) {  # an lm object
-                total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
-            } else {  # a glm object
-                total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
-            }
-            rbind(table, total_row)
-        }
-    )
+  result <- switch(
+    test,
+    "Rao" = table,
+    "LRT" = table,
+    "Chisq" = {
+      dfs <- table[, "Df"]
+      vals <- table[, dev.col] / scale * sign(dfs)
+      vals[dfs %in% 0] <- NA
+      vals[!is.na(vals) & vals < 0] <- NA  # rather than p = 0
+      total_row <- c("Total", sum(vals, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
+      rbind(
+        table[1:(nrow(table) - 1), ],  # Exclude last row (residuals)
+        total_row,
+        table[nrow(table), ]  # Add residuals row
+      )
+    },
+    "F" = {
+      dfs <- table[, "Df"]
+      Fvalue <- (table[, dev.col] / dfs) / scale
+      Fvalue[dfs %in% 0] <- NA
+      Fvalue[!is.na(Fvalue) & Fvalue < 0] <- NA  # rather than p = 0
+      total_row <- c("Total", sum(Fvalue, na.rm = TRUE), sum(abs(dfs), na.rm = TRUE), NA, NA, NA)
+      rbind(
+        table[1:(nrow(table) - 1), ],  # Exclude last row (residuals)
+        total_row,
+        table[nrow(table), ]  # Add residuals row
+      )
+    },
+    "Cp" = {
+      if ("RSS" %in% names(table)) {  # an lm object
+        total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
+      } else {  # a glm object
+        total_row <- c("Total", sum(table[, "Cp"], na.rm = TRUE))
+      }
+      rbind(
+        table[1:(nrow(table) - 1), ],  # Exclude last row (residuals)
+        total_row,
+        table[nrow(table), ]  # Add residuals row
+      )
+    }
+  )
+  return(result)
 }
 
 
